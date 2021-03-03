8ac66f6d37d73e38cfa2b107c31161d851066794
Bank_Watchara <watchara1589@gmail.com>
2021-03-04 00:50:01 +0700

    add another file

---
+++ podpod/Classes/podpod.h
@@ -0,0 +1,19 @@
+//
+//  podpod.h
+//  podpod
+//
+//  Created by mac_riverpark on 3/3/2564 BE.
+//  Copyright © 2564 BE CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for podpod.
+FOUNDATION_EXPORT double podpodVersionNumber;
+
+//! Project version string for podpod.
+FOUNDATION_EXPORT const unsigned char podpodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <podpod/PublicHeader.h>
+
+
---
+++ podpod/Classes/podpod.swift
@@ -0,0 +1,23 @@
+//
+//  podpod.swift
+//  podpod
+//
+//  Created by mac_riverpark on 4/3/2564 BE.
+//  Copyright © 2564 BE CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+public final class podpod {
+
+    let name = "podpod"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
