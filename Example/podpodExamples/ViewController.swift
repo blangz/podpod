a38bedbf47e53c34e731bcd24ca01ce3acd39974
Bank_Watchara <watchara1589@gmail.com>
2021-03-04 00:00:34 +0700

    Initial project setup

---
+++ Example/podpodExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  podpodExamples
+//
+//  Created by mac_riverpark on 3/3/2564 BE.
+//  Copyright © 2564 BE CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Example/podpodExamplesTests/podpodExamplesTests.swift
@@ -0,0 +1,34 @@
+//
+//  podpodExamplesTests.swift
+//  podpodExamplesTests
+//
+//  Created by mac_riverpark on 3/3/2564 BE.
+//  Copyright © 2564 BE CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import podpodExamples
+
+class podpodExamplesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
